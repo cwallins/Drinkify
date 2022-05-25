7cc52e0744b8f39dd3a5ec89474f5ba5f7ad42d3
cwallins <Wallin1996@gmail.com>
2022-05-25 10:16:33 +0200

    General changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav'
+import Search from './Search'
 
 function App() {
   return (
+
     <div className="App">
+      
+      <Nav />
+      <Search />
 
     </div>
   );
