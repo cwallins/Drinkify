64168db617ab55e85aa8789bfe4e12787ea1861a
cwallins <Wallin1996@gmail.com>
2022-05-27 10:49:13 +0200

    Funktionalitet för react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import Nav from './Nav'
 import Search from './Search'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
+import About from './About'
 
 function App() {
   return (
 
-    <div className="App">
-      
-      <Nav />
-      <Search />
+    <Router>
+      <div className="App">
+        
+        <Nav />
+        <Search />
 
-    </div>
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+
+
+      </div>
+    </Router>
   );
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,18 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Nav() {
     return (
 
         <div>
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <Link to="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
                     <svg class="bi me-2" width="40" height="32"></svg>
                     <b><span id="logo" class="fs-4">Drinkify</span></b>
                     <span id="drink" class="material-symbols-outlined">local_bar</span>
                     <br></br>
-                </a>
+                </Link>
                 <ul class="nav nav-pills">
-                    <b><li class="nav-item"><a href="#" id="links" class="nav-link">About Drinkify</a></li></b>
+                    <b><li class="nav-item"><Link to="/about" id="links" class="nav-link">About Drinkify</Link></li></b>
                 </ul>
             </header>
         </div>
