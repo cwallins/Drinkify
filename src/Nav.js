5534be4e69996d0d225ee015d6ae9b9fea71d73d
cwallins <Wallin1996@gmail.com>
2022-05-27 15:25:37 +0200

    tog bort onödig klass

--- src/Nav.js
+++ src/Nav.js
@@ -7,7 +7,6 @@ export default function Nav() {
         <div>
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                 <Link to="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    <svg class="bi me-2" width="40" height="32"></svg>
                     <b><span id="logo" class="fs-4">Drinkify</span></b>
                     <span id="drink" class="material-symbols-outlined">local_bar</span>
                     <br></br>
