674c16bcf50a729f9399f4ca99faf6b4aef3c4ff
antonsandman <75663580+antonsandman@users.noreply.github.com>
2022-05-30 17:27:34 +0200

    testing api fetching within the same comp

--- src/Drink.js
+++ src/Drink.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Drink(props) {
-    console.log('Hej');
+
     return (
         <div className='list-group-item'>
             {props.item.name}
--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,56 @@
-import React, { useState, useRef } from 'react'
-import Results from './Results'
+import React, { useState } from 'react'
+// import Drinks from './Drinks';
+import Drink from './Drink';
 
 export default function Search() {
 
+    const userInput = 'Margarita'; //arg for Drinks later
+
+    const [drinks, setDrinks] = useState([]);
+
+    async function getData(raq, p) {
+        // raq: resource and query, p: parameter
+        let res = await fetch(`https://thecocktaildb.com/api/json/v1/1/${raq}${p}`, { method: 'GET' });
+        return await res.json();
+    }
+
+    async function getDrinksByName(userInput) {
+        let data = await getData('search.php?s=', userInput);
+
+        let drink = data.drinks.map((drink) => {
+            return {
+                id: `${drink.idDrink}`,
+                name: `${drink.strDrink}`,
+                img: `${drink.strDrinkThumb}`,
+                instructions: `${drink.strInstructions}`
+            }
+        });
+        setDrinks(drink);
+    }
+
+    function handleClick() {
+        // kontrollera input
+        // const userInput = document.getElementById('user-input').val;
+        getDrinksByName(userInput);
+    }
+
     return (
-        <div className="search-container">
-            <div className="search">
-                <input type="text" placeholder="Have a drink..." className="search-field" />
-                <button onClick={Results} id="button"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path></svg></button>
+        <div>
+            <div className="search-container">
+                <div className="search">
+                    <input id="user-input" type="text" placeholder="Have a drink..." className="search-field" />
+                    <button onClick={handleClick} id="button"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path></svg></button>
+                </div>
+            </div>
+            <div className="drinks-container">
+                {drinks.map((drink, i) => {
+                    return (
+                        <div data-drink-id={drink.id}>
+                            <Drink key={i} item={drink} />
+                        </div>
+                    );
+                })}
             </div>
         </div>
-    )
+    );
 }
