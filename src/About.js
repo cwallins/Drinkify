d6b6df227c4077d7b6eceb62c81f4d03a4352b1c
cwallins <Wallin1996@gmail.com>
2023-02-26 15:46:27 +0100

    Updated About.js

--- src/About.js
+++ src/About.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
 export default function About() {
-	return (
-		<main className="about">
-			<div className="about-container">
-				<div className="search">
-					Welcome to Drinkify - Your cocktail database online!
-					<br />
-					<br />
-					This project aims to use APIs and frameworks to create a dynamic web application.
-					By using this site you can browse and store your favorite drinks in the web's local storage.
-					<br />
-					<br />
-					Happy Drink Hunting!
-				</div>
-			</div>
-		</main>
-	);
+  return (
+    <React.Fragment>
+      <main className="about">
+        <div className="about-container">
+          <div className="search">
+            Welcome to Drinkify - Your cocktail database online!
+            <br />
+            <br />
+            This project aims to use APIs and frameworks to create a dynamic web application.
+            <br />
+            By using this site you can browse and store your favorite drinks in the web's local storage.
+            <br />
+            <br />
+            Happy Drink Hunting!
+          </div>
+        </div>
+      </main>
+    </React.Fragment>
+  );
 }
