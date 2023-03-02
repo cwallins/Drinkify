05f140f03da92f47234f49d3a96166708dfe2a31
Christopher Wallin <80338047+cwallins@users.noreply.github.com>
2023-03-02 14:31:38 +0100

    Formatting Change

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import './App.css';
-import Search from './Search'
-import StoredDrinks from './StoredDrinks';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
-import About from './About'
+import "./App.css";
+import Search from "./Search";
+import StoredDrinks from "./StoredDrinks";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./About";
 
 function App() {
   return (
-
     <Router>
       <div className="App">
-
         <Search />
-
         <Switch>
           <Route exact path="/about">
             <About />
           </Route>
-          
           <Route exact path="/mydrinks">
             <StoredDrinks />
           </Route>
         </Switch>
-
-
       </div>
     </Router>
   );
