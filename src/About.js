0ddaa65028be7d106aa22d330e430613b1e3edad
Christopher Wallin <80338047+cwallins@users.noreply.github.com>
2023-03-06 12:29:18 +0100

    Minor updates

--- src/About.js
+++ src/About.js
@@ -6,12 +6,19 @@ export default function About() {
       <main className="about">
         <div className="about-container">
           <div className="search">
-            Welcome to Drinkify - Your cocktail database online!
+            Welcome to Drinkify 2.0 - Your cocktail database online!
             <br />
             <br />
-            This project aims to use APIs and frameworks to create a dynamic web application.
+            This project originally aimed to use APIs and frameworks to create a dynamic web application.
             <br />
             By using this site you can browse and store your favorite drinks in the web's local storage.
+            <br />
+            <br />
+            The project group is now re-structuring this project from last autumn - 22 for the course: Individuell Fördjupning.
+            The code has been re-written to follow React standards and has implemented standard hooks throughout to make it better. 
+            <br />
+            <br /> Furthermore, we have created a version with custom hooks that investigates if the use of those in a project like
+            this is benefitial. 
             <br />
             <br />
             Happy Drink Hunting!
--- src/Drink.js
+++ src/Drink.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React from "react";
 
 export default function Drink(props) {
-
   function addOrDelBtn(id) {
-
     let storage = localStorage.drinks;
     let newStorage;
-    storage === undefined ? newStorage = [] : newStorage = JSON.parse(storage);
+    storage === undefined
+      ? (newStorage = [])
+      : (newStorage = JSON.parse(storage));
 
     if (newStorage.length === 0) {
-      return <button className="btn btn-sm btn-success align-self-start" onClick={() => { props.addDrink(props.item) }}>add</button>
+      return (
+        <button
+          className="btn btn-sm btn-success align-self-start"
+          onClick={() => {
+            props.addDrink(props.item);
+          }}
+        >
+          Add
+        </button>
+      );
     } else {
-
       let isInStorage = false;
       for (let i = 0; i < newStorage.length; i++) {
         if (newStorage[i].id === id) {
@@ -20,30 +28,60 @@ export default function Drink(props) {
       }
 
       if (isInStorage === true) {
-        return <button className="btn btn-sm btn-danger align-self-start" onClick={() => { props.removeDrink(props.item.id) }}>X</button>
+        return (
+          <button
+            className="btn btn-sm btn-danger align-self-start"
+            onClick={() => {
+              props.removeDrink(props.item.id);
+            }}
+          >
+            Remove
+          </button>
+        );
       } else {
-        return <button className="btn btn-sm btn-success align-self-start" onClick={() => { props.addDrink(props.item) }}>add</button>
+        return (
+          <button
+            className="btn btn-sm btn-success align-self-start"
+            onClick={() => {
+              props.addDrink(props.item);
+            }}
+          >
+            Add
+          </button>
+        );
       }
     }
   }
 
   return (
     <React.Fragment>
-      <li className='list-group-item drink-list-item'>
-        <img src={props.item.img} alt={'Picture of ' + props.item.name} className='rounded'></img>
-        <div className='drink-info'>
-          <span><b><p>{props.item.name}</p></b></span>
+      <li className="list-group-item drink-list-item">
+        <img
+          src={props.item.img}
+          alt={"Picture of " + props.item.name}
+          className="rounded"
+        ></img>
+        <div className="drink-info">
+          <span>
+            <b>
+              <p>{props.item.name}</p>
+            </b>
+          </span>
           <div>
             <b>Ingredients: </b>
-            {props.item.ingredients.map((ingredient, i) => <span key={i}>{(i ? ", " : "") + ingredient}</span>)}
+            {props.item.ingredients.map((ingredient, i) => (
+              <span key={i}>{(i ? ", " : "") + ingredient}</span>
+            ))}
           </div>
           <div>
-            <b><p className='mb-0'>Instructions:</p></b>
+            <b>
+              <p className="mb-0">Instructions:</p>
+            </b>
             {props.item.instructions}
           </div>
         </div>
         {addOrDelBtn(props.item.id)}
       </li>
     </React.Fragment>
-  )
+  );
 }
